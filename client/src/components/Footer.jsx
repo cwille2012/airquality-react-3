4d43331a151e5a6f7f6135fe94852b6bf524b7de
Chris Wille <chris@christopherwille.com>
2018-04-22 06:11:19 -0400

    new template added

--- client/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => ( 
-        <div className='footer'>
-            Copyright © Christopher Wille
-        </div>
-)
-
-export default Footer
--- client/src/components/Users.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Users = () => (
-  <div>
-    <h3>Users</h3>
-  </div>
-)
-
-export default Users
